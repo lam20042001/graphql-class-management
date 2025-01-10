c69fdaec791433a5ba29789612c7a4fb1a686104
lam20042001 <lythanhlam12@gmail.com>
2025-01-10 11:30:50 +0700

    update error filter

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { GraphQLErrorFilter } from './filters/graphql-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4000', // Allow requests from this origin
-    methods: 'GET,POST,PUT,DELETE', // Allow these HTTP methods
-    allowedHeaders: 'Content-Type,Authorization', // Allow these headers
+    origin: 'http://localhost:4000',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
   });
+  app.useGlobalFilters(new GraphQLErrorFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 
